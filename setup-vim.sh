7c20122cbc8454dd8c806ad3f0fc191e7c6de637
Muhammed Haque <nohorjo@gmail.com>
2018-04-26 22:47:08 +0100

    Fixed theme get

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/colors/dracula.vim https://raw.githubusercontent.com/dracula/vim/master/colors/dracula.vim
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
-git submodule add git@github.com:dracula/vim.git ~/.vim/bundle/dracula-theme
+
