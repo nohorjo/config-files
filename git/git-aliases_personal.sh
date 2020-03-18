0b410c23ec899ec5c810bbfd1c07c3afe8477fdc
Muhammed Haque <nohorjo@gmail.com>
2020-03-18 09:53:19 +0000

    Fix issue tags for non issues

--- git/git-aliases_personal.sh
+++ git/git-aliases_personal.sh
@@ -3,9 +3,10 @@ bd() {
     git push origin --delete $1
 }
 commit-with-issue-tag() {
-    local issue_tag="#$(git symbolic-ref --short HEAD | cut -d- -f1) -"
-    if [ "$(echo $issue_tag | sed 's/[^0-9 \-#]//g')" = '' ]
+    if git symbolic-ref --short HEAD | grep -q '^[0-9]\+'
     then
+        local issue_tag="#$(git symbolic-ref --short HEAD | cut -d- -f1) -"
+    else
         local issue_tag=""
     fi
     git commit -m "$issue_tag $@"
