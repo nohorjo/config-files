1ce9170fb41974b8992b8bc8d9e820d34fe34d74
Muhammed Haque <nohorjo@gmail.com>
2021-05-06 13:29:40 +0100

    Update work commit-with-issue-tag

--- git/git-aliases_work.sh
+++ git/git-aliases_work.sh
@@ -1,3 +1,9 @@
 commit-with-issue-tag() {
-    git commit -m "$@"
+    if git symbolic-ref --short HEAD | grep -q '^[0-9]\+'
+    then
+        local issue_tag="$(git symbolic-ref --short HEAD | cut -d- -f1) "
+    else
+        local issue_tag=""
+    fi
+    git commit -m "$issue_tag$@"
 }
